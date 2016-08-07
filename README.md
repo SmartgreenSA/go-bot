c4528cb37133a2cd2ad9574ec1ecf111719dff32
Juliano Fernandes <julianofernandes@gmail.com>
2016-08-07 12:59:58 -0300

    Fix config file example (YAML tokens must be lower case) and adjust identation based on the editorconfig file settings.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # go-bot
-A simple multi-protocol robot written in Go. It's based on _go-chat-bot's_ [project](https://github.com/go-chat-bot) that supports [Slack](https://slack.com), [IRC](https://en.wikipedia.org/wiki/Internet_Relay_Chat) and [Telegram](https://telegram.org/) protocols.
+A simple multi-protocol robot written in Go.
+
+It's based on _go-chat-bot's_ [project](https://github.com/go-chat-bot) that supports [Slack](https://slack.com), [IRC](https://en.wikipedia.org/wiki/Internet_Relay_Chat) and [Telegram](https://telegram.org/) protocols.
--- main.go
+++ main.go
@@ -1,63 +1,64 @@
 package main
 
 import (
-	"log"
-	"os"
-	"io/ioutil"
-	"gopkg.in/yaml.v2"
+    "log"
+    "os"
+    "io/ioutil"
+    "gopkg.in/yaml.v2"
 
-	"github.com/go-chat-bot/bot/irc"
-	"github.com/go-chat-bot/bot/slack"
-	"github.com/go-chat-bot/bot/telegram"
-	_ "github.com/go-chat-bot/plugins-br/cnpj"
-	_ "github.com/go-chat-bot/plugins-br/cotacao"
-	_ "github.com/go-chat-bot/plugins-br/cpf"
-	_ "github.com/go-chat-bot/plugins-br/dilma"
-	_ "github.com/go-chat-bot/plugins-br/lula"
-	_ "github.com/go-chat-bot/plugins-br/megasena"
-	_ "github.com/go-chat-bot/plugins/9gag"
-	_ "github.com/go-chat-bot/plugins/catfacts"
-	_ "github.com/go-chat-bot/plugins/catgif"
-	_ "github.com/go-chat-bot/plugins/chucknorris"
-	_ "github.com/go-chat-bot/plugins/crypto"
-	_ "github.com/go-chat-bot/plugins/encoding"
-	_ "github.com/go-chat-bot/plugins/example"
-	_ "github.com/go-chat-bot/plugins/gif"
-	_ "github.com/go-chat-bot/plugins/godoc"
-	_ "github.com/go-chat-bot/plugins/guid"
-	_ "github.com/go-chat-bot/plugins/puppet"
-	_ "github.com/go-chat-bot/plugins/treta"
-	_ "github.com/go-chat-bot/plugins/url"
+    "github.com/go-chat-bot/bot/irc"
+    "github.com/go-chat-bot/bot/slack"
+    "github.com/go-chat-bot/bot/telegram"
+    _ "github.com/go-chat-bot/plugins-br/cnpj"
+    _ "github.com/go-chat-bot/plugins-br/cotacao"
+    _ "github.com/go-chat-bot/plugins-br/cpf"
+    _ "github.com/go-chat-bot/plugins-br/dilma"
+    _ "github.com/go-chat-bot/plugins-br/lula"
+    _ "github.com/go-chat-bot/plugins-br/megasena"
+    _ "github.com/go-chat-bot/plugins/9gag"
+    _ "github.com/go-chat-bot/plugins/catfacts"
+    _ "github.com/go-chat-bot/plugins/catgif"
+    _ "github.com/go-chat-bot/plugins/chucknorris"
+    _ "github.com/go-chat-bot/plugins/crypto"
+    _ "github.com/go-chat-bot/plugins/encoding"
+    _ "github.com/go-chat-bot/plugins/example"
+    _ "github.com/go-chat-bot/plugins/gif"
+    _ "github.com/go-chat-bot/plugins/godoc"
+    _ "github.com/go-chat-bot/plugins/guid"
+    _ "github.com/go-chat-bot/plugins/puppet"
+    _ "github.com/go-chat-bot/plugins/treta"
+    _ "github.com/go-chat-bot/plugins/url"
 )
 
 type Config struct {
-	Debug bool
-	SlackToken string
-	TelegramToken string
-	IRC irc.Config
+    Debug bool
+    SlackToken string
+    TelegramToken string
+    IRC *irc.Config
 }
 
 func main() {
-	config := loadConfig()
+    config := loadConfig()
 
-	go irc.Run(config.IRC)
-	go slack.Run(config.SlackToken)
-	telegram.Run(config.TelegramToken, config.Debug)
+    go irc.Run(config.IRC)
+    go slack.Run(config.SlackToken)
+    telegram.Run(config.TelegramToken, config.Debug)
 }
 
-func loadConfig() *Config {
-	filename := os.Args[1]
-	source, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic(err)
-	}
+func loadConfig() Config {
+    filename := os.Args[1]
+    source, err := ioutil.ReadFile(filename)
+    if err != nil {
+        panic(err)
+    }
+    log.Printf("%s\n", source)
 
-	var config Config
-	err = yaml.Unmarshal(source, &config)
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("Configuration loaded: %v\n", config)
+    var config Config
+    err = yaml.Unmarshal(source, &config)
+    if err != nil {
+        panic(err)
+    }
+    log.Printf("Configuration loaded: %v\n", config)
 
-	return &config
+    return config
 }
