88d61d2616061213b3418bd4b407d923ad5edfa0
Luiz de Prá <luiz@smartgreen.net>
2016-08-08 16:59:44 -0300

    Settling a Tab vs. Space Wars winner

--- config.go
+++ config.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "io/ioutil"
-    "gopkg.in/yaml.v2"
-    "github.com/go-chat-bot/bot/irc"
+	"fmt"
+	"github.com/go-chat-bot/bot/irc"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 )
 
 type Configuration interface {
-    LoadFromFile(filename string) error
+	LoadFromFile(filename string) error
 }
 
 type Config struct {
-    Debug bool
-    SlackToken string
-    TelegramToken string
-    IRC *irc.Config
+	Debug         bool
+	SlackToken    string
+	TelegramToken string
+	IRC           *irc.Config
 }
 
 func (c *Config) LoadFromFile(filename string) error {
-    source, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return err
-    }
+	source, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
 
-    err = yaml.Unmarshal(source, c)
-    if err != nil {
-        return err
-    }
+	err = yaml.Unmarshal(source, c)
+	if err != nil {
+		return err
+	}
 
-    log.Printf("Configuration loaded: %s\n", c)
+	log.Printf("Configuration loaded: %s\n", c)
 
-    return nil
+	return nil
 }
 
 func (c *Config) String() string {
-    return fmt.Sprintf("{%t %s %s %v}", c.Debug, c.SlackToken, c.TelegramToken, c.IRC)
+	return fmt.Sprintf("{%t %s %s %v}", c.Debug, c.SlackToken, c.TelegramToken, c.IRC)
 }
--- main.go
+++ main.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-    "os"
+	"os"
 
-    "github.com/go-chat-bot/bot/irc"
-    "github.com/go-chat-bot/bot/slack"
-    "github.com/go-chat-bot/bot/telegram"
-    _ "github.com/go-chat-bot/plugins-br/cnpj"
-    _ "github.com/go-chat-bot/plugins-br/cotacao"
-    _ "github.com/go-chat-bot/plugins-br/cpf"
-    _ "github.com/go-chat-bot/plugins-br/dilma"
-    _ "github.com/go-chat-bot/plugins-br/lula"
-    _ "github.com/go-chat-bot/plugins-br/megasena"
-    _ "github.com/go-chat-bot/plugins/9gag"
-    _ "github.com/go-chat-bot/plugins/catfacts"
-    _ "github.com/go-chat-bot/plugins/catgif"
-    _ "github.com/go-chat-bot/plugins/chucknorris"
-    _ "github.com/go-chat-bot/plugins/crypto"
-    _ "github.com/go-chat-bot/plugins/encoding"
-    _ "github.com/go-chat-bot/plugins/example"
-    _ "github.com/go-chat-bot/plugins/gif"
-    _ "github.com/go-chat-bot/plugins/godoc"
-    _ "github.com/go-chat-bot/plugins/guid"
-    _ "github.com/go-chat-bot/plugins/puppet"
-    _ "github.com/go-chat-bot/plugins/treta"
-    _ "github.com/go-chat-bot/plugins/url"
+	"github.com/go-chat-bot/bot/irc"
+	"github.com/go-chat-bot/bot/slack"
+	"github.com/go-chat-bot/bot/telegram"
+	_ "github.com/go-chat-bot/plugins-br/cnpj"
+	_ "github.com/go-chat-bot/plugins-br/cotacao"
+	_ "github.com/go-chat-bot/plugins-br/cpf"
+	_ "github.com/go-chat-bot/plugins-br/dilma"
+	_ "github.com/go-chat-bot/plugins-br/lula"
+	_ "github.com/go-chat-bot/plugins-br/megasena"
+	_ "github.com/go-chat-bot/plugins/9gag"
+	_ "github.com/go-chat-bot/plugins/catfacts"
+	_ "github.com/go-chat-bot/plugins/catgif"
+	_ "github.com/go-chat-bot/plugins/chucknorris"
+	_ "github.com/go-chat-bot/plugins/crypto"
+	_ "github.com/go-chat-bot/plugins/encoding"
+	_ "github.com/go-chat-bot/plugins/example"
+	_ "github.com/go-chat-bot/plugins/gif"
+	_ "github.com/go-chat-bot/plugins/godoc"
+	_ "github.com/go-chat-bot/plugins/guid"
+	_ "github.com/go-chat-bot/plugins/puppet"
+	_ "github.com/go-chat-bot/plugins/treta"
+	_ "github.com/go-chat-bot/plugins/url"
 )
 
 func main() {
-    filename := os.Args[1]
-    
-    var config Config
-    err := config.LoadFromFile(filename)
-    if err != nil {
-        panic(err)
-    }
+	filename := os.Args[1]
 
-    go irc.Run(config.IRC)
-    go slack.Run(config.SlackToken)
-    telegram.Run(config.TelegramToken, config.Debug)
+	var config Config
+	err := config.LoadFromFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	go irc.Run(config.IRC)
+	go slack.Run(config.SlackToken)
+	telegram.Run(config.TelegramToken, config.Debug)
 }
